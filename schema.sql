7649fa9d91cf31008df5f75b9c0d35a012b52617
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2024-01-03 18:53:37 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,31 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
 
 -- SQLite schema for simplified book application
 
--- Create a 'books' table
-CREATE TABLE books (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL
+-- Create a 'books' table with auto-incrementing id
+CREATE TABLE public.books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL
 );
 
+
+ALTER TABLE public.books OWNER TO postgres;
+
 -- Insert sample data
-INSERT INTO books (title, author) VALUES ('1984', 'George Orwell');
-INSERT INTO books (title, author) VALUES ('To Kill a Mockingbird', 'Harper Lee');
+INSERT INTO public.books (title, author) VALUES ('1984', 'George Orwell');
+INSERT INTO public.books (title, author) VALUES ('To Kill a Mockingbird', 'Harper Lee');
